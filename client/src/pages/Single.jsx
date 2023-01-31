ace1f8f188485514c8fd87547f8d43a84b2d4abf
datdtnhcse <dat.dtnhcse@gmail.com>
2023-01-31 22:40:17 +0700

    sth on single page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className='content'>content
+        <img src="" alt=""></img>
+        <div className="user">
+          <img src="" alt=""></img>
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+        </div>
+      </div>
+      <div className='menu'>menu</div>
+    </div>
   )
 }
 
